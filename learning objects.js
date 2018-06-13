af985a9801ede5500e75dbf395feaea11afbda4b
Анастасия <nchuklinova@MacBook-Air-Anastasia-2.local>
2018-06-13 23:53:04 +0300

    learning objects

---
+++ learning objects.js
@@ -0,0 +1,10 @@
+var g = {};
+var anna = {
+	first_name: "Anna",
+	last_name: "Mikhailova",
+	age: 35,
+	print: function(){
+		console.log(this.first_name, this.last_name,"is", this.age);
+	}
+};
+anna.print();
