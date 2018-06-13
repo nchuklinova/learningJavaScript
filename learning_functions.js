c732617bc427755ff0c1b557cec72d60b4ac3b8d
Анастасия <nchuklinova@MacBook-Air-Anastasia-2.local>
2018-06-13 23:31:31 +0300

    variable function

--- learning_functions.js
+++ learning_functions.js
@@ -1,7 +1,19 @@
 function sum(a, b){
 	return a + b;
 }
+sum(7, 6);
 
 function minus(a, b){
 	return a - b;
 }
+
+var t = 6;
+var y = "stop";
+var k = function(){
+	console.log("SOS");
+};
+
+k();
+
+
+
