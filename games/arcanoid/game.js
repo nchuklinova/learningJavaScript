1cbec2cf05f9503f095ed57bdbe687ad4062ef6d
grigdevelop <grigdevelop@gmail.com>
2018-06-16 13:46:16 +0200

    zabludshiy sharik

--- games/arcanoid/game.js
+++ games/arcanoid/game.js
@@ -1,18 +1,46 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(0, 0, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(240, 160, 60, 0, Math.PI*2);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.stroke();
+var x = canvas.width / 2;
+var y = canvas.height /2;
 
-ctx.closePath();
+var dx = 2;
+var dy = -2;
+
+var ballRadius = 50;
+
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	drawBall();
+
+	if(y + dy < 0 + ballRadius) {
+    	dy = -dy;
+	}
+
+	if(x + dx > canvas.width - ballRadius){
+		dx = -dx;
+	}
+
+	if(x + dx < 0 + ballRadius){
+		dx = -dx;
+	}
+
+	if(y + dy > canvas.height - ballRadius){
+		dy = -dy;
+	}
+
+	x += dx;
+    y += dy; 
+}
+
+function drawBall(){
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+setInterval(draw, 5);
